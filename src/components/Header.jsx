3de11975951601a8a078c38f2cdb5f89614cf24b
kamalpatel00 <57323096+kamalpatel00@users.noreply.github.com>
2020-04-20 18:21:51 +0530

    color changes on scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,76 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PhoneMissedIcon from "@material-ui/icons/PhoneMissed";
 import "./Style.css";
+import { MenuItem } from "@material-ui/core";
 class Header extends Component {
-  state = {
-    color: "white",
-  };
-
-  listenScrollEvent = (e) => {
-    if (window.scrollY > 400) {
-      this.setState({ color: "black" });
-    } else {
-      this.setState({ color: "white" });
-    }
-  };
-
-  componentDidMount() {
-    window.addEventListener("scroll", this.listenScrollEvent);
+  constructor() {
+    super();
+    this.state = {
+      scrolled: false,
+    };
   }
 
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== true) {
+        this.setState({ scrolled: true });
+      } else {
+        this.setState({ scrolled: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll");
+  }
   render() {
     return (
-      <div className="Header" style={{ color: this.state.color }}>
-        <img
-          className="logo"
-          src="https://cdn.greenhouse.io/external_greenhouse_job_boards/logos/000/003/977/original/BettercomGreenhouse-1024x587.png?1554404619"
-        />
-        <ul className="menu-ul">
-          {/* <li>
+      <div className="hdiv">
+        <div className={this.state.scrolled ? "nav scrolled" : "nav"}>
+          <img
+            className="logo"
+            src="https://cdn.greenhouse.io/external_greenhouse_job_boards/logos/000/003/977/original/BettercomGreenhouse-1024x587.png?1554404619"
+          />
+
+          <ul className="menu-ul">
+            {/* <li>
             <Link to="/"> Home</Link>
           </li> */}
-          <li>
-            <Link to="/about"> Credit score</Link>
-          </li>
-          <li id="con">
-            <Link to="/contact">+91-9648877586</Link>
-          </li>
-        </ul>
+            <li>
+              <Link to="/about">
+                <MenuItem
+                  style={{
+                    paddingLeft: 7,
+                    textDecoration: "none",
+                    color: "black",
+                    marginTop: 0,
+                  }}
+                >
+                  Credit score
+                </MenuItem>
+              </Link>
+            </li>
+            <li id="con">
+              <Link to="/contact">
+                <MenuItem
+                  style={{
+                    paddingLeft: 7,
+                    marginTop: 0,
+
+                    textDecoration: "none",
+                    color: "black",
+                  }}
+                >
+                  <PhoneMissedIcon fontSize="small" />
+                  <span> </span>
+                  +91-9648877586
+                </MenuItem>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
